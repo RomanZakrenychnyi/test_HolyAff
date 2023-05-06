7796b619e06be4ee9548c30c8c5805aa9488562b
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-05-06 10:08:03 +0300

    add user answers to LS, and on click at third modal whrite in console all the user answers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,22 @@
 const modalBtn = document.querySelector("#p_modal_button3");
 const questionsAnswers = document.querySelector("#content1");
+
 const LS_KEY = "user_answers";
+const userAnswersArr = [];
 
 const handleAnswersColect = (event) => {
-  event.preventDefault();
-    const selectedValue = event.target.textContent;
+  const selectedValue = event.target.textContent;
+  //   selectedValue = userAnswersArr.replace(/(\r\n|\n|\r)/gm, '');
+  userAnswersArr.push(selectedValue);
+  JSON.stringify(localStorage.setItem(LS_KEY, userAnswersArr));
+  console.log(userAnswersArr);
+};
 
-
-  JSON.stringify(localStorage.setItem(LS_KEY, selectedValue));
-  console.log(selectedValue);
+const handleOKClick = () => {
+  const userData = localStorage.getItem(LS_KEY);
+  localStorage.setItem(LS_KEY, "");
+  console.log(userData);
 };
 
 questionsAnswers.addEventListener("click", handleAnswersColect);
+modalBtn.addEventListener("click", handleOKClick);
