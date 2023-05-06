22831c715136163dd6b9bb2acbc6017d1247c2b6
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-05-06 15:27:28 +0300

    now you can add anonymous comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,76 @@
-const modalBtn = document.getElementById('p_modal_button3');
-const questionsAnswers = document.getElementById('content1');
+const modalBtn = document.getElementById("p_modal_button3");
+const questionsAnswers = document.getElementById("content1");
 
-const publishBtn = document.getElementById('publish-btn');
-const commentsInput = document.getElementById('comment-text');
-const commentsFace = document.getElementsByClassName('comments_face');
-console.log(commentsFace);
+const publishBtn = document.getElementById("publish-btn");
+const commentsInput = document.getElementById("comment-text");
+const commentsFace = document.querySelector(".comments_face");
 
+// Save user answers in LS; output the answers to the browser console by pressing OK btn;
 
-// Save user answers in LS; output the answers to the browser console by pressing OK btn; 
-
-const LS_KEY = 'user_answers';
+const LS_KEY = "user_answers";
 const userAnswersArr = [];
 
 const handleAnswersColect = (event) => {
-  const selectedValue = event.target.textContent;
-  //   selectedValue = userAnswersArr.replace(/(\r\n|\n|\r)/gm, '');
+  let selectedValue = event.target.textContent
+    .replaceAll("\n", "")
+    .replaceAll(" ", "");
+  console.log(selectedValue);
   userAnswersArr.push(selectedValue);
-  JSON.stringify(localStorage.setItem(LS_KEY, userAnswersArr));
+
+  localStorage.setItem(LS_KEY, JSON.stringify(userAnswersArr));
   console.log(userAnswersArr);
 };
 
 const handleOKClick = () => {
   const userData = localStorage.getItem(LS_KEY);
-  localStorage.setItem(LS_KEY, '');
+  localStorage.setItem(LS_KEY, "");
   console.log(userData);
 };
 
-questionsAnswers.addEventListener('click', handleAnswersColect);
-modalBtn.addEventListener('click', handleOKClick);
+questionsAnswers.addEventListener("click", handleAnswersColect);
+modalBtn.addEventListener("click", handleOKClick);
 
 // Add comment
 
 const handleSetComment = () => {
-    const commentEl = document.createElement('div');
-    commentEl.classList.add('new-comment');
+  // create div container for new comment
+  const commentEl = document.createElement("div");
+  commentEl.classList.add("comments");
 
-    const commentText = document.createTextNode(commentsInput.value);
-    commentEl.appendChild(commentText);
+  // create div el for user img
+  const profileEl = document.createElement("div");
+  profileEl.classList.add("profile");
+  profileEl.style.cssText = "width:60px;height:45px;";
 
-    commentsFace.appendChild(commentEl);
+  // create user img el
+  const profileImg = document.createElement("img");
+  profileImg.src =
+    "https://w7.pngwing.com/pngs/565/454/png-transparent-user-computer-icons-anonymity-head-miscellaneous-face-service.png";
+  profileEl.appendChild(profileImg);
+  commentEl.appendChild(profileEl);
 
-    commentsInput.value = '';
-}
+  // create div for comment text output
+  const commentContent = document.createElement("div");
+  commentContent.classList.add("comment-content");
 
-publishBtn.addEventListener('click', handleSetComment)
+  const newUserName = document.createElement("p");
+  newUserName.classList.add("name");
+  newUserName.textContent = "anonymous";
+
+  const commentText = document.createElement("p");
+  commentText.textContent = commentsInput.value;
+
+  commentContent.appendChild(newUserName);
+  commentContent.appendChild(commentText);
+
+  //   commentEl.appendChild(commentContent);
+  commentEl.appendChild(commentContent);
+
+  // const commentText = document.createTextNode(commentsInput.value);
+
+  commentsFace.appendChild(commentEl);
+
+  commentsInput.value = "";
+};
+
+publishBtn.addEventListener("click", handleSetComment);
